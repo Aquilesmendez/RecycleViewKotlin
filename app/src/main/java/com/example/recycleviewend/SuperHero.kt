d5534ffe4ffa0ddf35b10d257b9ad5903a949953
Aquiles Mendez <aquilesmendez@gmail.com>
2023-06-13 10:29:10 -0400

    Initial commit

---
+++ app/src/main/java/com/example/recycleviewend/SuperHero.kt
@@ -0,0 +1,9 @@
+package com.example.recycleviewend
+
+class SuperHero (
+
+    val superhero:String,
+    val publisher:String,
+    val realName:String,
+    val photo:String,
+)
---
+++ app/src/main/java/com/example/recycleviewend/SuperHeroProvider.kt
@@ -0,0 +1,17 @@
+package com.example.recycleviewend
+
+class SuperHeroProvider {
+    companion object{
+
+        val superHeroList = listOf<SuperHero>(
+            SuperHero(
+                "nakama",
+                "Toei",
+                "oda",
+                "https://www.example.com"),
+
+
+        )
+    }
+
+}
